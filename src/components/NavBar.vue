213a2fed51ba8e0a7d87accc75646463aa725777
hiki89 <markohitl89@gmail.com>
2018-08-21 18:41:05 +0200

    Zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <router-link class="navbar-brand" to="/movies">Movies App</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
