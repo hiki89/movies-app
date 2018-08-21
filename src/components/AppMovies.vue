7ba17b17fa8af3fbb6cd490f2515ba98593fe111
hiki89 <markohitl89@gmail.com>
2018-08-21 19:58:19 +0200

    Zadatak 4

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
         <h1>Movies</h1>
+        <movie-row :movies="movies"/>
     </div>
 </template>
 
 <script>
+import MovieRow from '../components/MovieRow'
+import { movies } from '../services/Movies'
+
 export default {
-    
+    components: {
+        MovieRow
+    },
+    data() {
+        return {
+            movies: []
+        }
+    },
+
+    beforeRouteEnter (to, from, next) {
+        movies.getAll()
+            .then((response) => {
+                next((vm) => {
+                    vm.movies = response.data
+                })
+            })
+    },
 }
 </script>
 
--- src/services/Movies.js
+++ src/services/Movies.js
@@ -2,6 +2,22 @@ import axios from 'axios'
 
 export default class Movies {
     constructor() {
-        axios.defaults.baseURL = 'http://localhost:3000/api'
+        axios.defaults.baseURL = 'http://localhost:3000/api/'
+    }
+
+    getAll() {
+        return axios.get('movies')
+        // return new Promise((resolve) => {
+        //     setTimeout(() => {
+        //         resolve([
+        //             {
+        //                 id: 1,
+        //                 name: 'Admin'
+        //             }
+        //         ])
+        //     }, 2000)
+        // })
     }
 }
+
+export const movies = new Movies()
